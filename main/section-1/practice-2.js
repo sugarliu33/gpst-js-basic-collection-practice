628ea4610525658e1df5559d4a65380761644622
sugarliu <450755513@qq.com>
2017-08-22 10:05:52 +0800

    use filter and includes to modify the function

--- main/section-1/practice-2.js
+++ main/section-1/practice-2.js
@@ -1,21 +1,11 @@
 'use strict';
 
-function includes(collection, ch) {
-    for (let item of collection) {
-        if (item === ch) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 module.exports = function collectSameElements(collectionA, collectionB) {
     let result = [];
-    for (let item of collectionA) {
-        if (includes(collectionB[0], item)) {
-            result.push(item);
+    result = collectionA.filter(function (item) {
+        if (collectionB[0].includes(item)) {
+           return item;
         }
-    }
+    });
     return result;
 }
