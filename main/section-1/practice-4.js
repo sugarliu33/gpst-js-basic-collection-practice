56d1bade6b0db77ab067e87f6e19fb9be4702c35
sugarliu <450755513@qq.com>
2017-08-22 10:15:54 +0800

    use filter and includes to modify the function

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -1,21 +1,11 @@
 'use strict';
 
-function includes(collection, ch) {
-    for (let item of collection) {
-        if (item === ch) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 module.exports = function collectSameElements(collectionA, objectB) {
     let result = [];
-    for (let item of collectionA) {
-        if (includes(objectB.value, item.key)) {
+    collectionA.filter(function (item) {
+        if (objectB.value.includes(item.key)) {
             result.push(item.key);
         }
-    }
+    });
     return result;
 }
